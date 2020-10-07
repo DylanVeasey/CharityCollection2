5531d40fc41d5db9543741f2fb6ae4f230c43d28
Dylan Veasey <43819824+DylanVeasey@users.noreply.github.com>
2020-10-07 16:45:56 +0100

    Finished Project

---
+++ CharityCollection2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharityCollection2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int TotalAmountCollected;
+
+            UserInput User1 = new UserInput();
+            UserInput User2 = new UserInput();
+            UserInput User3 = new UserInput();
+
+            User1.GetAmount(1);
+            User2.GetAmount(2);
+            User3.GetAmount(3);
+
+            TotalAmountCollected = User1.AmountCollected + User2.AmountCollected + User3.AmountCollected;
+
+            if (TotalAmountCollected < 100)
+            {
+                Console.WriteLine("A total of {0} was raised.", TotalAmountCollected);
+                TotalAmountCollected = TotalAmountCollected + 100;
+                Console.WriteLine("With the company bonus, this is {0}" ,TotalAmountCollected);
+            }
+            else if( TotalAmountCollected >= 1000 && TotalAmountCollected <= 2000)
+            {
+                Console.WriteLine("A total of {0} was raised.", TotalAmountCollected);
+                TotalAmountCollected = TotalAmountCollected * 2;
+                Console.WriteLine("With the company bonus, this is {0}", TotalAmountCollected);
+            }
+            else
+            {
+                Console.WriteLine("A total of {0} was raised.", TotalAmountCollected);
+                TotalAmountCollected = (TotalAmountCollected - 2000) + 4000;
+                Console.WriteLine("With the company bonus, this is {0}", TotalAmountCollected);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CharityCollection2/UserInput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharityCollection2
+{
+    class UserInput
+    {
+        private int _amountCollected;
+
+        public int AmountCollected
+        {
+            get { return _amountCollected; }
+            set { _amountCollected = value; }
+        }
+
+        public void GetAmount(int IndexOfPerson)
+        {
+            Console.WriteLine("How much did person {0} collect?", IndexOfPerson);
+            AmountCollected = Int32.Parse(Console.ReadLine());
+        }
+
+    }
+}
